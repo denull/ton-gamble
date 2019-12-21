94f017f3d74da1a2b4a5313d74425eaddac0af42
Denis Olshin <me@denull.ru>
2019-12-21 17:01:59 +0300

    moved compile-func to tests

--- compile-func.sh
+++
@@ -1,2 +0,0 @@
-func -o"code-getters.fif" -P ../src/crypto/smartcont/stdlib.fc code.fc getters.fc  
-func -o"code.fif" -P ../src/crypto/smartcont/stdlib.fc code.fc
---
+++ tests/compile-func.sh
@@ -0,0 +1,2 @@
+func -o"../code-getters.fif" -P ../../src/crypto/smartcont/stdlib.fc ../code.fc ../getters.fc  
+func -o"../code.fif" -P ../../src/crypto/smartcont/stdlib.fc ../code.fc
