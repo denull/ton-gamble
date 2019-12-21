7da9badbf167054ea683796b935184635d555cb9
Denis Olshin <me@denull.ru>
2019-12-22 00:52:03 +0300

    added pause to init test

--- tests/test-init.sh
+++ tests/test-init.sh
@@ -1,2 +1,3 @@
 ../init.fif 0 gamble
+read -p "Send 1 Gram to the non-bouncable address above and press [Enter] to continue"
 ../../build/lite-client/lite-client  -C ../../ton-lite-client-test1.config.json -c"sendfile gamble-init-query.boc"
