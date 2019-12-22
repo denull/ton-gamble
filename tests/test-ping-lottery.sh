e942a21ba13035da0d9103afd10ce862939c03b1
Denis Olshin <me@denull.ru>
2019-12-22 11:54:27 +0300

    not returning money from archived games

---
+++ tests/test-ping-lottery.sh
@@ -0,0 +1,2 @@
+../make-move.fif kQDyJbwG0-KJX1hnR0kwmuzn50qBfyv2dNdo63zFL-rLYn92 lottery-entry-1 $1 $2 0
+../../build/lite-client/lite-client  -C ../../ton-lite-client-test1.config.json -c"sendfile move-query.boc"
